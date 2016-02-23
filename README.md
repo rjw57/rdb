9987cbe99695f1de7ff46c4e274af8854629f209
Rich Wareham <rich@richwareham.com>
2016-02-23 17:13:00 +0000

    Initial commit of empty file

---
+++ README.md
@@ -0,0 +1,1 @@
+# Experimental sql.js GUI
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var isProduction = false;
+
+var plugins = [
+  new ExtractTextPlugin("[name].css"),
+  new webpack.optimize.DedupePlugin(),
+  new HtmlWebpackPlugin({ title: "Rich's DB" }),
+];
+
+if(isProduction) {
+  plugins = plugins.concat([
+    new webpack.DefinePlugin({ "process.env": { "NODE_ENV": '"production"' } }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { unused: true, dead_code: true, warnings: false },
+    }),
+  ]);
+}
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./entry.js",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel",
+        query: { presets: ["react", "es2015"], },
+      },
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      // For Bootstrap
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"},
+    ]
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+  devtool: "source-map",
+  plugins: plugins,
+}
