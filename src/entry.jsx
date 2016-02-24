d980272ef5398079571100b025a76b0e0bce150f
Rich Wareham <rich@richwareham.com>
2016-02-24 00:10:53 +0000

    Add logger support to redux

--- src/entry.jsx
+++ src/entry.jsx
@@ -2,20 +2,29 @@
 require('expose?$!expose?jQuery!jquery');
 require('bootstrap-webpack');
 
-import { createStore } from "redux";
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-
-import rdbApp from "./reducers";
-import App from "./components/app.jsx";
-
-// Create the redux store for the application
-let store = createStore(rdbApp);
-
 // Create the DOM element which will contain our app
 let containerElem = document.createElement('div');
 document.body.appendChild(containerElem);
+
+// Create the redux store for the application
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+
+import reducer from './reducers';
+
+const logger = createLogger();
+const store = createStore(reducer, applyMiddleware(
+    thunk, promise, logger
+));
+
+// Render the Application
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './components/app.jsx';
 
 render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
 
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var isProduction = false;
 
 var plugins = [
-  new ExtractTextPlugin("[name].css"),
+  new ExtractTextPlugin('[name].css'),
   new webpack.optimize.DedupePlugin(),
-  new HtmlWebpackPlugin({ title: "Rich's DB" }),
+  new HtmlWebpackPlugin({ title: 'Rich\'s DB' }),
   new webpack.optimize.CommonsChunkPlugin({
-    name: "sql.js", filename: "sql.js",
+    name: 'sql.js', filename: 'sql.js',
   }),
 ];
 
 if(isProduction) {
   plugins = plugins.concat([
-    new webpack.DefinePlugin({ "process.env": { "NODE_ENV": '"production"' } }),
+    new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': '"production"' } }),
     new webpack.optimize.UglifyJsPlugin({
       exclude: /sql\.js$/,
       compress: { unused: true, dead_code: true, warnings: false },
@@ -26,14 +26,14 @@ if(isProduction) {
 }
 
 module.exports = {
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname, 'src'),
   entry: {
-    app: "./entry.jsx",
-    "sql.js": ["sql.js"],
+    app: './entry.jsx',
+    'sql.js': ['sql.js'],
   },
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "bundle.js",
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   module: {
     noParse: [ /sql\.js\/.*\.js$/ ],
@@ -41,24 +41,24 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel",
-        query: { presets: ["react", "es2015"], },
+        loader: 'babel',
+        query: { presets: ['react', 'es2015'], },
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
       },
       // For Bootstrap
-      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"},
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
     ]
   },
   watchOptions: {
     aggregateTimeout: 300,
     poll: 1000,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: plugins,
 }
